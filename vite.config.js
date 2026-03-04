aa3bd1ab9072b1a9235a13b7c563ec100191a63a
migmig3030 <migueldedeus09@gmail.com>
2026-03-04 16:39:07 -0300

    Adiciona CNAME e ajusta base path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Mig3030/-site-nutricionista/', // Substitua pelo nome que você der ao repositório no GitHub
+  base: '/', // Apenas uma barra para domínio próprio!
 })
